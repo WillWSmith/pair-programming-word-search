abddae83fa02864709c50e47a567da54ad3d58b8
Will Smith <will.w.smith94@gmail.com>
2023-12-15 15:10:56 -0500

    can search for a given word both vertically and horizontally

--- wordsearch.js
+++ wordsearch.js
@@ -1,24 +1,30 @@
 const wordSearch = (letters, word) => {
-    // Check if the matrix is null, has no rows, or the rows have no columns
-    if (word === '' || !letters || letters.length === 0 || letters[0].length === 0) {
-        return false;
+        
+    if (word === "" || letters.length === 0 || letters[0].length === 0 || typeof word !== `string`) {
+        return false; 
     }
+    
+    word = word.toUpperCase();
 
-    // Join each row to form a string and check horizontally
-    const horizontalJoin = letters.map(ls => ls.join(''));
-    for (const l of horizontalJoin) {
-        if (l.includes(word)) return true;
+    const horizontalJoin = letters.map(ls => ls.join(''))
+    for (let l of horizontalJoin) {
+        if (l.includes(word)) 
+        return true;
     }
-
-    // Check vertically
+  
+   
     for (let i = 0; i < letters[0].length; i++) {
         const verticalJoin = letters.map(ls => ls[i]).join('');
-        if (verticalJoin.includes(word)) return true;
+        if (verticalJoin.includes(word)) 
+        return true;
     }
 
-    // Return false if the word is not found in either direction
+
     return false;
+
 };
+
+
 
 
 module.exports = wordSearch
